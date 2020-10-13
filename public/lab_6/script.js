8e95a363499a5df0c8e060682b9251bb73bde929
jmontano98 <lilcooljeff123@gmail.com>
2020-10-13 16:50:06 -0400

    First Lab 6 commit

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -11,26 +11,58 @@ function range(int) {
 function sortFunction(a, b, key) {
   if (a[key] < b[key]) {
     return -1;
-  } if (a[key] > b[key]) {
+  }
+  if (a[key] > b[key]) {
     return 1;
   }
   return 0;
 }
 
-document.body.addEventListener('submit', async (e) => {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+document.body.addEventListener("submit", async (e) => {
   e.preventDefault(); // this stops whatever the browser wanted to do itself.
   const form = $(e.target).serializeArray(); // here we're using jQuery to serialize the form
-  fetch('/api', {
-    method: 'POST',
+  fetch("/api", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(form)
+    body: JSON.stringify(form),
   })
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-      console.log('fromServer', fromServer);
+      const randCountries = range(10);
+      const totalCountries = fromServer.length;
+
+      for (let i = 0; i < randCountries.length; i++) {
+        randCountries[i] = fromServer[getRandomInt(totalCountries)];
+      }
+
+      randCountries.sort(function (a, b) {
+        return a.name.localeCompare(b.name);
+    });
+
+      if ($("ul")[0]) {
+        $("ul").empty();
+      } else {
+        $("form").prepend("<ul class='flex-inner'></ul>");
+      }
+      for (let i = 0; i < randCountries.length; i++) {
+        $("ul").prepend(
+          "<li><input type='checkbox' id='country' name=" +
+            randCountries[i].code +
+            " value=" +
+            randCountries[i].code +
+            "><label for='country'>" +
+            randCountries[i].name +
+            "</label></li>"
+        );
+      }
+
+      console.log("fromServer", fromServer);
     })
     .catch((err) => console.log(err));
 });
